23ae75f302a6d9f438d9f46962a39550ac02560c
Roberto Guzmán <roberto.guzman@eossoftware.mx>
2021-01-29 15:56:46 -0600

    tester update state

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -2,15 +2,80 @@ import React, {useState, useContext, useEffect} from 'react';
 import {GiftedChat} from 'react-native-gifted-chat';
 import database from '@react-native-firebase/database';
 import {AuthContext} from '../navigation/AuthProvider';
+import { TextComponent } from 'react-native';
 
+//componente que estan exportando.
 export default function ChatScreen() {
+  //hook useContext
   const {user} = useContext(AuthContext);
   const currentUser = user.toJSON();
+
+  //es un hook useState();
+  //[ miVariable, función para actualizar mi variable ] useState(elValorInicialDeMiVariable);
   const [messages, setMessages] = useState([]);
 
-  function handleSend(messages = []) {
-    const text = messages[0].text;
+  // []); => se ejecute una sola vez cuando se abre el componente.
+  // [user]); => se ejecuta cada vez que el usuario cambia.
+  // [user, messages]); => se ejecuta cada vez que el usuario cambia y cada vez que los mensajes cambian.
 
+  //Hook
+  useEffect(() => {
+    //uno auno de los mensajes
+    database()
+      .ref('chat/')
+      .orderBy('fecha')
+      .on('child_added', (snapshot) => {
+        console.log('User data: ', snapshot.val());
+
+        //setMessages(snapshot.val());
+
+        //no se deben perder los mensajes
+        //Inicio = []
+        //1er = [msj1];
+        //2do = [msj1, msj2];
+        //setMessages([...messages, snapshot.val()]);
+
+        //CallBack
+        //prevenir que una actualización se cumplió.
+
+        /*snapshot.val() = {
+          fecha: timestamp
+          texto: "texto",
+          uid: 2345
+        }
+        */
+
+        /*1er. [] => [{
+          fecha: timestamp
+          texto: "texto",
+          uid: 2345
+        }]
+        */
+
+        /*2do. 
+          [{
+            fecha: timestamp
+            texto: "texto",
+            uid: 2345
+          }] => 
+          [..., {
+          fecha: timestamp2
+          texto: "texto2",
+          uid: 2345-2
+        }]
+        */
+        setMessages((prevState) => [...prevState, snapshot.val()]);
+        //estoy actualizando 1, espera
+          //ahora actualiza 2
+        //estoy actualizando 2, espera
+          //ahora actualiza 3
+        //ya actualice 3
+          //ahora actualiza 4
+        //ya actualice 1
+      });
+  }, []);
+
+  function handleSend(messages = []) {
     database()
       .ref('chat/')
       .push({
@@ -22,42 +87,6 @@ export default function ChatScreen() {
         console.log('mensaje guardado');
       })
       .catch((e) => console.log(e));
-
-    useEffect(() => {
-      const messagesListener = database()
-        .ref('chat/')
-        .orderBy('fecha')
-        .on('child_added', (snapshot) => {
-          console.log('User data: ', snapshot.val());
-          setMessages(snapshot.val);
-        });
-      /* 
-        .on((querySnapshot) => {
-          const messages = querySnapshot.docs.map((doc) => {
-            const firebaseData = doc.data();
-
-            const data = {
-              _id: doc.id,
-              text: '',
-              createdAt: new Date().getTime(),
-              ...firebaseData,
-            };
-
-            if (!firebaseData.system) {
-              data.user = {
-                ...firebaseData.user,
-                name: firebaseData.user.email,
-              };
-            }
-
-            return data;
-          });
-
-          setMessages(messages);
-        }); */
-
-      return () => messagesListener();
-    }, []);
   }
 
   return (
